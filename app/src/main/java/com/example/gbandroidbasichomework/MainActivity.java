27dd7120d3457b1b61dde0729ec04fe68bdfa4da
vergik <mihailovpeter@gmail.com>
2021-01-11 00:19:52 +0300

    Исправлены некоторые недочеты: отступы, форматирование, хакрдкод.

--- app/src/main/java/com/example/gbandroidbasichomework/MainActivity.java
+++ app/src/main/java/com/example/gbandroidbasichomework/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 startActivity(intent);
             }
         });
